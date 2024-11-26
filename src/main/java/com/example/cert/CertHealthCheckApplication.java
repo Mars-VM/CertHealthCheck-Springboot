20f423b63167c6d7848d61d333641e252ff7028c
mars.zhu <mars.zhu@valtech.com>
2024-11-26 12:52:03 +0800

    init

---
+++ src/main/java/com/example/cert/CertHealthCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.cert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertHealthCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertHealthCheckApplication.class, args);
+	}
+
+}
